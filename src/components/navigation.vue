6212df969eaa7f24bb836cbd9795eceb096a0cfc
名称 <>
2021-03-22 14:18:55 +0800

    导航栏样式更新
    .
    
    Co-Authored-By: PriscillaYeah <80497947+PriscillaYeah@users.noreply.github.com>

--- src/components/navigation.vue
+++ src/components/navigation.vue
@@ -2,10 +2,10 @@
   <nav>
     <ul>
       <li>
-        <router-link to="/" exact>Export</router-link>
-        <router-link to="/page1" exact>Contact</router-link>
-        <router-link to="/page2" exact>Personal</router-link>
-        <router-link to="/page3" exact>Info</router-link>
+        <router-link to="/" exact>HOME</router-link>
+        <router-link to="/page1" exact>CONTACT</router-link>
+        <router-link to="/page2" exact>PERSONAL</router-link>
+        <img src="../assets/lingdang.png">
       </li>
     </ul>
   </nav>
@@ -15,37 +15,46 @@
 export default {
   name: 'navigation',
 }
+
 </script>
 
 <style scoped>
 ul {
   list-style-type: none; /*消除黑点*/
-  text-align: center;
   margin: 0;
   height: 60px; /*导航栏高度*/
 }
 li {
   display: inline-block;
   margin: 0 10px;
-  margin-top: 21px; /*上边距*/
+  margin-top: 17px; /*上边距*/
   text-align: right;
 
 }
+img{
+  width: 18px;
+  text-align: right;
+  margin-right: 28px;
+  margin-left: 10px;
+}
+
 
 a {
   color: rgba(48, 70, 92, 100); /*设置字体颜色*/
   text-decoration: none; /*消除下划线*/
-  font-size: 18px; /*文字大小*/
+  font-size: 16px; /*文字大小*/
   padding: 19px;  /*单词间距*/
   
   border-radius: 5px;
   left: 1104px;
+  font-family: Roboto;
 
 }
 nav {
   background:white;
   left: 0px;
   top: 0px;
+  text-align: right;
 
 }
 /*选中则发生一些变化*/
